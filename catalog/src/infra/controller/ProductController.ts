4c005f96f7a6b3c52a8cf6dc10ae3360d682a99c
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-09-24 22:06:01 -0300

    feat: add ProductController with endpoint /products to get and post

---
+++ catalog/src/infra/controller/ProductController.ts
@@ -0,0 +1,26 @@
+import AddProduct from "../../application/usecase/AddProduct";
+import GetProducts from "../../application/usecase/GetProducts";
+import HttpServer from "../http/HttpServer";
+
+export default class ProductController {
+  constructor(
+    readonly httpServer: HttpServer,
+    readonly getProducts: GetProducts,
+    readonly addProduct: AddProduct,
+  ) {
+    this.httpServer.on("get", "/products", async () => {
+      const products = await getProducts.execute();
+      return {
+        statusCode: 200,
+        body: products,
+      };
+    });
+
+    this.httpServer.on("post", "/products", async (body: any) => {
+      await addProduct.execute(body);
+      return {
+        statusCode: 201,
+      };
+    });
+  }
+}
