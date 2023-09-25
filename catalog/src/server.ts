1694f98f4a4cb1f36bb7ed646051c4fd464c07ba
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-09-24 22:07:05 -0300

    feat: add server to start application

---
+++ catalog/src/server.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import ExpressAdapter from "./infra/http/ExpressAdapter";
+import GetProducts from "./application/usecase/GetProducts";
+import AddProduct from "./application/usecase/AddProduct";
+import ProductController from "./infra/controller/ProductController";
+import ProductRepositoryDatabase from "./infra/repository/ProductRepositoryDatabase";
+
+const PORT = process.env.PORT || 3001;
+
+const httpServer = new ExpressAdapter();
+const prisma = new PrismaClient();
+const productRepository = new ProductRepositoryDatabase(prisma);
+const getProduct = new GetProducts(productRepository);
+const addProduct = new AddProduct(productRepository);
+new ProductController(httpServer, getProduct, addProduct);
+
+httpServer.listen(PORT);
