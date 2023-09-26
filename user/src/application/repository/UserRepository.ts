05c8d1ed07c2a23e9dfbc6bfa3e941bf265db9ed
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-09-26 15:10:04 -0300

    chore: fix variable name

--- user/src/application/repository/UserRepository.ts
+++ user/src/application/repository/UserRepository.ts
@@ -1,5 +1,5 @@
 import User from "../../domain/entities/User";
 
-export default interface SubscriptionRepository {
+export default interface UserRepository {
   save(user: Omit<User, "id">): Promise<void>;
 }
