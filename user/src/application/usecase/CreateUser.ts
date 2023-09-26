d49ae44f2e6ef52b5f73ee2b5c3a1e9002ac15e0
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-09-26 15:09:50 -0300

    feat: add bcrypt to hashed the password

--- user/src/application/usecase/CreateUser.ts
+++ user/src/application/usecase/CreateUser.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import UserRepository from "../repository/UserRepository";
 
 type Input = {
@@ -10,6 +11,8 @@ export default class CreateUser {
   constructor(readonly userRepository: UserRepository) {}
 
   async execute(input: Input) {
-    return await this.userRepository.save(input);
+    const hashedPassword = bcrypt.hashSync(input.password, 10);
+    const user = { ...input, password: hashedPassword };
+    return await this.userRepository.save(user);
   }
 }
