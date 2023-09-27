dc62098d42baddacf1f1e8aad558a2287511097f
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-09-27 08:37:54 -0300

    feat: add dockerfile and docker compose to run all app and fix binaryTargets in prisma

--- api-gateway/server.ts
+++ api-gateway/server.ts
@@ -5,6 +5,9 @@ import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 
 const PORT = process.env.PORT || 3001;
+const API_CATALOG = process.env.API_CATALOG || "localhost";
+const API_CHECKOUT = process.env.API_CHECKOUT || "localhost";
+const API_USER = process.env.API_USER || "localhost";
 
 const app = express();
 
@@ -23,9 +26,9 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 app.use(cors());
-app.use("/catalog", proxy("http://localhost:3002"));
-app.use("/checkout", proxy("http://localhost:3003"));
-app.use("/user", proxy("http://localhost:3004"));
+app.use("/catalog", proxy(`http://${API_CATALOG}:3002`));
+app.use("/checkout", proxy(`http://${API_CHECKOUT}:3003`));
+app.use("/user", proxy(`http://${API_USER}:3004`));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
